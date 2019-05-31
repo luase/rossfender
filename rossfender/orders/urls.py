6ab319df285e1580f529771e75f5479ba87a894a
Jaime Alonso <a.lonso@yahoo.com>
2019-05-31 00:54:49 -0500

    Order Index beta ready

--- rossfender/orders/urls.py
+++ rossfender/orders/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 path('', views.index_view, name='index'),
 path('create/', views.create_order, name='create_order'),
 path('consult/', views.consult, name='consult'),
+path('order/<int:pk>/', views.order_view, name='order'),
 ]
--- rossfender/orders/views.py
+++ rossfender/orders/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 import datetime
 
 def index_view(request):
-    orders_for_today = Order.objects.filter(delivery_date__lte=datetime.date.today())
+    relevant_orders = Order.objects.filter(delivery_date__gt=datetime.date.today()).exclude(delivery_date__gte=datetime.date.today() + datetime.timedelta(days=8))
+    todays_orders = Order.objects.filter(delivery_date=datetime.date.today())
+    tomorrows_orders = Order.objects.filter(delivery_date=datetime.date.today() + datetime.timedelta(days=1))
     context = {
-        'orders_for_today' : orders_for_today,
+        'relevant_orders' : relevant_orders,
+        'todays_orders' : todays_orders,
+        'tomorrows_orders' : tomorrows_orders,
     }
     return render(request, 'orders/index.html', context)
+
+def order_view(request):
+    return 0
 
 def create_order(request):
     return 0
