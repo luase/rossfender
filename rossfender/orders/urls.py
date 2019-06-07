afcb8fb2498f868e9a12e88f51e3077690811b24
Jaime Alonso <a.lonso@yahoo.com>
2019-06-06 23:02:06 -0500

    Cambios masivos a las vistas.

--- rossfender/orders/urls.py
+++ rossfender/orders/urls.py
@@ -6,8 +6,9 @@ from . import views
 app_name = 'orders'
 urlpatterns = [
 path('', views.index_view, name='index'),
-path('order/form/', views.order_form, name='form'),
-path('order/create/', views.order_create, name='create'),
+path('order/list/', views.order_list, name='order_list'),
+path('order/form/', views.order_form, name='order_form'),
+# path('order/create/', views.order_create, name='create'),
 # path('consult/', views.consult, name='consult'),
-path('order/<int:pk>/', views.order_view, name='detail'),
+path('order/<int:pk>/', views.order_view.as_view(), name='order_detail'),
 ]
--- rossfender/orders/views.py
+++ rossfender/orders/views.py
@@ -3,20 +3,20 @@ from .models import Order
 from django.shortcuts import render
 import datetime
 from .forms import OrderForm
+from django.views import generic
 
 def index_view(request):
-    relevant_orders = Order.objects.filter(delivery_date__gt=datetime.date.today()).exclude(delivery_date__gte=datetime.date.today() + datetime.timedelta(days=8))
     todays_orders = Order.objects.filter(delivery_date=datetime.date.today())
     tomorrows_orders = Order.objects.filter(delivery_date=datetime.date.today() + datetime.timedelta(days=1))
     context = {
-        'relevant_orders' : relevant_orders,
         'todays_orders' : todays_orders,
         'tomorrows_orders' : tomorrows_orders,
     }
     return render(request, 'orders/index.html', context)
 
-def order_view(request):
-    return 0
+class order_view(generic.DetailView):
+    model = Order
+    template_name = 'orders/order_detail.html'
 
 def order_form(request):
     # if this is a POST request we need to process the form data
@@ -36,8 +36,16 @@ def order_form(request):
 
     return render(request, 'orders/order_form.html', {'form': form})
 
-def order_create(request):
-    a = []
+# def order_create(request):
+#     a = []
 
-def consult(request):
-    return 0
+# def consult(request):
+#     return 0
+
+def order_list(request):
+    relevant_orders = Order.objects.filter(delivery_date__gte=datetime.date.today())
+    # exclude(delivery_date__gte=datetime.date.today() + datetime.timedelta(days=8))
+    context = {
+        'order_list' : relevant_orders,
+    }
+    return render(request, 'orders/order_list.html', context)
